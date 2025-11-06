5d727b9c60e192b5aea96352d692144db265d33d
Daniel Sanchez Velasquez <daniel.sanchez.velasquez090@gmail.com>
2025-11-06 01:26:34 -0500

    1.1

--- exportation.py
+++ exportation.py
@@ -145,9 +145,8 @@ def guardado_idr():
             if orden_de_origen:
                 cur.execute('SELECT * FROM _ingresos_y_salidas WHERE numero_de_orden = %s', (orden_de_origen,))
                 listed_order = cur.fetchone()
-                return jsonify({'error': f'Si Se Activo: {listed_order} - {orden_de_origen}'}), 400 # MANDA ERROR.
                 
-                if not listed_order:
+                if listed_order is None:
                     return jsonify({'error': 'El número de *Orden de Origen de la Garantía* no se encuentra registrado como un ingreso previo existente.'}), 400 # MANDA ERROR.
 
         try:
