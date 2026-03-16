c14797c36542dc7c9cdb815b68e05b385065340c
Daniel Sanchez Velasquez <daniel.sanchez.velasquez090@gmail.com>
2026-03-16 13:07:15 -0700

    Update exportation.py

--- exportation.py
+++ exportation.py
@@ -270,7 +270,7 @@ def coordinacion_exportacion_general():
         cur= conn.cursor()
 
         try:
-            normalized_columns= ('responsable', 'break', 'tratamiento', 'numero_del_ciclo', 'usuario')
+            normalized_columns= ('responsable', 'break', 'tratamiento', 'numero_del_ciclo', 'usuario', 'area')
             column_list = []
             valeu_list = []
             for column, valeu in data.items():
