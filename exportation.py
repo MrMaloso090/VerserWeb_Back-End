b1452839b5f99858dcc24b32c15e82a98bda38e5
Daniel Sanchez Velasquez <daniel.sanchez.velasquez090@gmail.com>
2026-03-29 15:32:54 -0700

    Update exportation.py

--- exportation.py
+++ exportation.py
@@ -337,7 +337,7 @@ def verificacion_de_autenticidad(usuario):
 
 # FUNCION QUE SE ENCARGA DE VERIFICAR LA HORA Y FILTRAR CUALQUIER INGRESO DE INFORMACION QUE NO SE HAYA REALIZADO DENTRO DE LOS PRIMEROS 15 MINUTOS DE CADA HORA.
 def verificacion_de_hora(usuario):
-    if usuario is 'Admin': return False
+    if usuario == 'Admin': return False
 
     minuto_actual = datetime.now(pytz.timezone('America/Bogota')).minute
     if minuto_actual > 15:
@@ -348,7 +348,7 @@ def verificacion_de_hora(usuario):
 
 # LIMITE QUE NO PERMITE INGRESAR 2 VECES A UN MISMO USUARIO EN LA MISMA HORA.
 def limite_de_ingresos_por_hora(usuario):
-    if usuario is 'Admin': return False
+    if usuario == 'Admin': return False
 
     with mysql.connector.connect(**connection) as conn:
         cur = conn.cursor(buffered=True)
