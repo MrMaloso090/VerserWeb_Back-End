21822027af10818b9144986fd8facaa5f5c5b110
Daniel Sanchez Velasquez <daniel.sanchez.velasquez090@gmail.com>
2026-03-29 16:14:09 -0700

    Update exportation.py

--- exportation.py
+++ exportation.py
@@ -366,4 +366,4 @@ def limite_de_ingresos_por_hora(usuario):
             else: return False
         
     except Exception as e:
-        return jsonify({'error': e}), 400
+        return jsonify({'error': str(e)}), 400
