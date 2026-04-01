1f93011307e5145da0cbb81c3ece991581e63de3
Daniel Sanchez Velasquez <daniel.sanchez.velasquez090@gmail.com>
2026-04-01 05:33:21 -0700

    Update exportation.py

--- exportation.py
+++ exportation.py
@@ -278,8 +278,8 @@ def coordinacion_exportacion_general():
         if verificacion_de_hora(usuario) is True:
             return jsonify({'error': 'No se puede ingresar información después de los primeros 15 minutos de cada hora'}), 400
         
-        if limite_de_ingresos_por_hora(usuario, table) is True:
-            return jsonify({'error': 'El usuario solo puede ingresar información una vez por hora'}), 400
+        #if limite_de_ingresos_por_hora(usuario, table) is True:
+            #return jsonify({'error': 'El usuario solo puede ingresar información una vez por hora'}), 400
     except Exception as e:
         return jsonify({'error': f'ERROR INESPERADO: \n{e}'}), 400
 
